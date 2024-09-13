afe8bb5491243e5a89b055d1e68835169121c794
Muhammad Ravat <m0ravat736@gmail.com>
2024-09-13 22:59:45 +0100

    refactored projectsRef code

--- my-first-react-app/src/MainPages/Pages/Projects/ProjectsRef.jsx
+++ my-first-react-app/src/MainPages/Pages/Projects/ProjectsRef.jsx
@@ -8,15 +8,12 @@ export function ProjectsRef({websites}){
                     {/* Left Side (Text and Links) */}
                     <div className="flex flex-col  w-full md:w-auto mr-4">
                         <span className="flex items-center space-x-2">
-                        <h1 className="font-bold text-large">{title}</h1>
-                        <a href={webLink} target="_blank" rel="noopener noreferrer"><i className="fa-solid fa-link"></i></a>
-                        <a href={gitLink} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
+                            <h1 className="font-bold text-large">{title}</h1>
+                            <a href={webLink} target="_blank" rel="noopener noreferrer"><i className="fa-solid fa-link"></i></a>
+                            <a href={gitLink} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
                         </span>
-                        <p className="mt-2 text-standard">
-                        {desc}
-                        </p>
+                        <p className="mt-2 text-standard">{desc}</p>
                     </div>
-
                     {/* Image (Aligned Right on larger screens, full width on small screens) */}
                     <img src={img} alt={title} className="w-full md:w-[60%] md:object-cover mt-4 md:my-auto" />
                 </div>
@@ -47,14 +44,11 @@ export function ProjectsRef2({projects}){
                     {/* Left Side (Text and Links) */}
                     <div className="flex flex-col  w-full md:w-auto mr-4">
                         <span className="flex items-center space-x-2">
-                        <h1 className="font-bold text-large">{title}</h1>
-                        <a href={gitLink} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
+                            <h1 className="font-bold text-large">{title}</h1>
+                            <a href={gitLink} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
                         </span>
-                        <p className="mt-2 text standard">
-                        {desc}
-                        </p>
+                        <p className="mt-2 text standard">{desc}</p>
                     </div>
-
                     {/* Image (Aligned Right on larger screens, full width on small screens) */}
                     <img src={img} alt={title} className="w-full md:w-[60%] md:object-cover mt-4 md:my-auto" />
                 </div>
