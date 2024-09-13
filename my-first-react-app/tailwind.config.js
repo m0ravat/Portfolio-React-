ac2a4cc81d73eb545ff71452d8a1df45c4c0bf9e
Muhammad Ravat <m0ravat736@gmail.com>
2024-09-13 22:13:35 +0100

    added font size classes

--- my-first-react-app/tailwind.config.js
+++ my-first-react-app/tailwind.config.js
@@ -10,6 +10,11 @@ export default {
       backgroundImage: {
         'mr-image': "url('./Components/MR.png')",
       },
+      fontSize: {
+        small: 'calc(9px + 0.390625vw)', // For small text
+        standard: 'calc(15px + 0.390625vw)', // For standard text
+        large: 'calc(25px + 0.390625vw)', // For large text
+      },
       colors: {
         // Add custom colors for dark mode if needed
         'dark-bg': '#1a1a1a', // Example color
