d4a7db00accc9ee25bae286bbecc3c5a10c20d74
Muhammad Ravat <m0ravat736@gmail.com>
2024-09-24 15:58:46 +0100

    altered project desc

--- my-first-react-app/src/MainPages/Pages/Projects/projects.jsx
+++ my-first-react-app/src/MainPages/Pages/Projects/projects.jsx
@@ -12,18 +12,18 @@ import { ProjectSlides } from "./Slides"
 import { ProjectsRef, ProjectsRef2 } from "./ProjectsRef"
 function Project(){
     const highlights = [
-        {img:notes, title:"MyNotesToDo App", desc:"This is my app where you can create projects with tasks and colour coded dates to keep up with which need to be done. You can see the details of it ",
+        {img:notes, title:"MyNotesToDo App", desc:"This is my app where you can create projects with tasks and colour coded dates to keep up with which need to be done. The tech stack used was: HTML/CSS/JS, GitHub, Vercel, SquareSpace Domains. You can see the details of it ",
             link:"#notes", index:0
         },
-        {img:port, title:"Personal Portfolio", desc:"This is my personal portfolio, which displays all relevant information about me. You can see more information by clicking ",
+        {img:port, title:"Personal Portfolio", desc:"This is my personal portfolio, which displays all relevant information about me. The tech stack used for this is: React, GitHub, Vercel, SquareSpace Domains. You can see more information by clicking ",
             link:"#port", index:1
         },
-        {img:module, title:"Module Enrolment System", desc:"This is my first large scale project in java, allowing a student to enrol on a class of his choosing. For more information click ",
+        {img:module, title:"Module Enrolment System", desc:"This is my first large scale project in java, allowing a student to enrol on a class of his choosing. This was created using Java via IntelliJ IDE. For more information click ",
             link:"#module", index:2
         },
     ]
     const websites = [
-        {title:"Portfolio", desc:"A website designed to showcase my web design and web development skills, while progressively using skills I have learnt through the Odin Project, from vanilla HTML/CSS and JS to using tools like Webpack, Vercel, React and TailwindCSS.",
+        {title:"Portfolio", desc:"A website designed to showcase my web design and web development skills, while progressively using skills I have learnt through the Odin Project, from vanilla HTML/CSS and JS to using tools like Webpack, Vercel, React and TailwindCSS, it's designed to give potential employers a brief look into my skills as a developer.",
             webLink:"https://www.moravat.me/", gitLink:"https://github.com/m0ravat/webPortfolio", img:react, id:"port"
         },
         {title:"ToDo App", desc:"This was a website I created that allows users to create tasks for projects and date them to keep up with them, seperating ongoing ones to finished tasks. It stores all data in localStorage, and implements basic CRUD techniques. It also has a form for user input and was the first program which showcases my data management skills.",
@@ -32,12 +32,12 @@ function Project(){
         {title:"Weather App", desc:"This was a basic weather app I made to demonstrate my use of API's. I used the visual crossing API to get the weather when the address is inputted, and I used the GIPHY API to generate a gif upon the pressing of a button. I also tailored it to mobile resolution by centering it for bigger resolutions.",
             webLink:"https://weather-app-steel-mu.vercel.app/",gitLink:"https://github.com/m0ravat/WeatherApp", img:api, id:"weather"
         },
-        {title:"CV Generator",desc:"This was my first project in React, where I prompt a user to give some inputs through a form, which generates a generic CV showing a person's introduction, their experience and skills. It was designed to practice some basic principles within react, such as state management and the use of props.",
+        {title:"CV Generator",desc:"This was my first project in React, where I prompt a user to give some inputs through a form, which generates a generic CV showing a person's introduction, their experience and skills. It was designed to practice some basic principles within react, such as state management and the use of prop validation, which helped in securing the foundation of using React.",
             webLink:"https://cv-maker-vert-seven.vercel.app/", gitLink:"https://github.com/m0ravat/CVMaker", img:react2, id:"cv"
         },
     ]
     const projects = [
-        {title:"Module Enrolment", desc:"A program designed to allow a student to enrol on one of multiple classes, and choosing a seat. This demonstrated my OOP skills by using a Module class, a Seat class and a Person class. It was designed on IntelliJ on Java and was inspired by a similar plane booking system I did at university.",
+        {title:"Module Enrolment", desc:"A program designed to allow a student to enrol on one of multiple classes, and choosing a seat. This demonstrated my OOP skills by using a Module class, a Seat class and a Person class. It was designed on IntelliJ in Java and was inspired by a similar plane booking system I did at university.",
             gitLink:"https://github.com/m0ravat/Enrol", img:java, id:"module"
         },
     ]
