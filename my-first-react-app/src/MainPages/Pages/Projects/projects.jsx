74ac5a0e3800bf79ff1af1dd948616e9a291d91f
Muhammad Ravat <m0ravat736@gmail.com>
2024-09-24 15:53:10 +0100

    added project images

--- my-first-react-app/src/MainPages/Pages/Projects/projects.jsx
+++ my-first-react-app/src/MainPages/Pages/Projects/projects.jsx
@@ -1,10 +1,14 @@
 import TopNav from "../../Components/TopNav/topnav"
 import Footer from "../../Components/footer"
 import java from './Media/javaImg.jpeg'
-import notes from './Media/logo.png'
-import port from './Media/portfolioImg.jpeg'
-import python from './Media/pythonImg.jpeg'
+import notes from './Media/Website_Images/Mynotes.png'
+import port from './Media/Website_Images/Portfolio.png'
+import trio from './Media/Website_Images/mix.png'
 import web from './Media/webImg.jpeg'
+import module from './Media/Project_Images/Module.png'
+import react from  './Media/Website_Images/React.png'
+import api from './Media/Website_Images/API.png'
+import react2 from './Media/Website_Images/reactt.png'
 import { ProjectSlides } from "./Slides"
 import { ProjectsRef, ProjectsRef2 } from "./ProjectsRef"
 function Project(){
@@ -15,22 +19,22 @@ function Project(){
         {img:port, title:"Personal Portfolio", desc:"This is my personal portfolio, which displays all relevant information about me. You can see more information by clicking ",
             link:"#port", index:1
         },
-        {img:java, title:"Module Enrolment System", desc:"This is my first large scale project in java, allowing a student to enrol on a class of his choosing. For more information click ",
+        {img:module, title:"Module Enrolment System", desc:"This is my first large scale project in java, allowing a student to enrol on a class of his choosing. For more information click ",
             link:"#module", index:2
         },
     ]
     const websites = [
         {title:"Portfolio", desc:"A website designed to showcase my web design and web development skills, while progressively using skills I have learnt through the Odin Project, from vanilla HTML/CSS and JS to using tools like Webpack, Vercel, React and TailwindCSS.",
-            webLink:"https://www.moravat.me/", gitLink:"https://github.com/m0ravat/webPortfolio", img:python, id:"port"
+            webLink:"https://www.moravat.me/", gitLink:"https://github.com/m0ravat/webPortfolio", img:react, id:"port"
         },
         {title:"ToDo App", desc:"This was a website I created that allows users to create tasks for projects and date them to keep up with them, seperating ongoing ones to finished tasks. It stores all data in localStorage, and implements basic CRUD techniques. It also has a form for user input and was the first program which showcases my data management skills.",
-            webLink:"https://www.mynotestodo.com/",gitLink:"https://github.com/m0ravat/ToDoListApp",img:web, id:"notes"
+            webLink:"https://www.mynotestodo.com/",gitLink:"https://github.com/m0ravat/ToDoListApp",img:trio, id:"notes"
         },
         {title:"Weather App", desc:"This was a basic weather app I made to demonstrate my use of API's. I used the visual crossing API to get the weather when the address is inputted, and I used the GIPHY API to generate a gif upon the pressing of a button. I also tailored it to mobile resolution by centering it for bigger resolutions.",
-            webLink:"https://weather-app-steel-mu.vercel.app/",gitLink:"https://github.com/m0ravat/WeatherApp", img:web, id:"weather"
+            webLink:"https://weather-app-steel-mu.vercel.app/",gitLink:"https://github.com/m0ravat/WeatherApp", img:api, id:"weather"
         },
         {title:"CV Generator",desc:"This was my first project in React, where I prompt a user to give some inputs through a form, which generates a generic CV showing a person's introduction, their experience and skills. It was designed to practice some basic principles within react, such as state management and the use of props.",
-            webLink:"https://cv-maker-vert-seven.vercel.app/", gitLink:"https://github.com/m0ravat/CVMaker", img:web, id:"cv"
+            webLink:"https://cv-maker-vert-seven.vercel.app/", gitLink:"https://github.com/m0ravat/CVMaker", img:react2, id:"cv"
         },
     ]
     const projects = [
